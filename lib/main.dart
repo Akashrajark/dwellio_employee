094134b1a82397f5c91d672b86ea67eb5ba55e90
jishnu6241 <jishnuov2002@gmail.com>
2025-03-02 02:42:26 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: appTheme, home: const LoginScreen());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: LoginScreen(),
+    );
   }
 }
