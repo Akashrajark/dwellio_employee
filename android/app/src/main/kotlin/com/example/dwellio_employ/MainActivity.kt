7f65928fdbaf7e635d8fa1b9c64e15c716d225d1
jishnu6241 <jishnuov2002@gmail.com>
2025-03-02 02:21:49 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/dwellio_employ/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dwellio_employ
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/util/check_login.dart
@@ -0,0 +1,15 @@
+import 'package:dwellio_employ/features/Login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void checkLogin(BuildContext context) {
+  Future.delayed(const Duration(milliseconds: 100), () {
+    User? currentUser = Supabase.instance.client.auth.currentUser;
+    if (!(currentUser != null &&
+        (currentUser.appMetadata['role'] == 'admin'))) {
+      Navigator.of(
+        context,
+      ).pushReplacement(MaterialPageRoute(builder: (context) => LoginScreen()));
+    }
+  });
+}
